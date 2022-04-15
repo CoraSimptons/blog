8afc1d15d93aba3f9e34299231c05167785c03bd
pseudoname <nnmt.lfg.2020@gmail.com>
2022-04-15 18:19:49 +0700

    Use static files and scss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// static file
+app.use(express.static(path.join(__dirname, 'public')))
+
 // http logger
 app.use(morgan('combined'))
 
@@ -14,7 +17,7 @@ app.engine('hbs', engine(
   {extname: '.hbs'}
 ))
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname,'resources/views'))
+app.set('views', path.join(__dirname, 'resources/views'))
 
 // Route
 app.get('/', (req, res) => {
