4cb0eff58702d5fde8f654333188e471392eec6c
pseudoname <nnmt.lfg.2020@gmail.com>
2022-04-20 16:37:30 +0700

    Create route, controller folder and use

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 // static file
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -27,23 +29,8 @@ app.engine('hbs', engine(
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-// Route
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body.gender)
-  res.send('')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
