ab293a04afa83c6cc84637136e1f3e603cf8e5da
pseudoname <nnmt.lfg.2020@gmail.com>
2022-04-27 12:10:08 +0700

    Update - CRUD

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -24,6 +25,9 @@ app.use(
 // js gửi lên (xmlhttprequest, axios)
 app.use(express.json());
 
+//
+app.use(methodOverride('_method'));
+
 // http logger
 //app.use(morgan('combined'))
 
@@ -32,7 +36,12 @@ app.engine(
     'hbs',
     engine(
         // congig file name
-        { extname: '.hbs' },
+        {
+            extname: '.hbs',
+            helpers: {
+                sum: (a, b) => a + b,
+            },
+        },
     ),
 );
 app.set('view engine', 'hbs');
